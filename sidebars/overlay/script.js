bae7d17e411b8935dc193bf848f89c69732e91d6
Cleomir <cleomir.mg@hotmail.com>
2020-08-15 12:07:49 +0300

    Add overlay side nav

---
+++ sidebars/overlay/script.js
@@ -0,0 +1,12 @@
+const sideNav = document.querySelector("#side-nav");
+const main = document.querySelector("#main");
+
+document.querySelector("span").addEventListener("click", () => {
+  sideNav.style.width = "250px";
+  main.style.marginLeft = "250px";
+});
+
+document.querySelector(".close-btn").addEventListener("click", () => {
+  sideNav.style.width = "0";
+  main.style.marginLeft = "0";
+});
